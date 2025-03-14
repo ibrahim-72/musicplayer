4fc4a602f64511b8eda624b5d32ebbb3cd1090c8
ibrahim-72 <140600839+ibrahim-72@users.noreply.github.com>
2025-03-15 01:06:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const playMusic = (track, pause=false) => {
 }
 
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
@@ -89,7 +89,7 @@ async function displayAlbums(){
         if(e.href.includes("/songs")){
             let folder = (e.href.split("/").slice(-2)[0])
             // Get the meta data of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="cs" class="card">
